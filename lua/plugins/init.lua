170998ada078f11f7fa56a4cc909296c227008e6
heart-hash <joeistyping@protonmail.com>
2024-03-29 21:21:34 -0400

    added harpoon2

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,6 +8,25 @@ return {
   },
 
   {
+    "theprimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon"):setup()
+    end,
+    keys = {
+      { "<leader>F", function() require("harpoon"):list():append() end, desc = "harpoon file", },
+      { "<leader>f", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
+      { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1", },
+      { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2", },
+      { "<leader>3", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3", },
+      { "<leader>4", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4", },
+      { "<leader>P", function() require("harpoon"):list():prev() end, desc = "harpoon to prev file", },
+      { "<leader>N", function() require("harpoon"):list():next() end, desc = "harpoon to next file", },
+    },
+  },
+
+  {
     "christoomey/vim-tmux-navigator",
     cmd = {
       "TmuxNavigateLeft",
