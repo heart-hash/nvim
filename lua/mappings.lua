78d699dac1ae6a8fe5377bd80ad54390afbfe83e
heart-hash <joeistyping@protonmail.com>
2024-03-29 21:36:59 -0400

    undotree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,11 +9,13 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Tmux Navigator
- map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "window left" })
- map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "window down" })
- map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "window up" })
- map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "window right" })
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "window left" })
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "window down" })
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "window up" })
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "window right" })
 
+-- undotree
+map("n", "<leader>n", "<cmd>UndotreeToggle<CR>", { desc = "Toggle undotree"})
 
 -- Keep the search term in the middle of the screen with n and N
 map("n", "n", "nzzzv", { desc = "Next search result centered" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,16 @@
 return {
+
+  {
+    "mbbill/undotree",
+    lazy = false, -- needs to be explicitly set, because of the keys property
+    cmd = {
+      "UndotreeToggle",
+    },
+  },
+  -- Override plugin definition options
+
+
+
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
@@ -15,14 +27,63 @@ return {
       require("harpoon"):setup()
     end,
     keys = {
-      { "<leader>F", function() require("harpoon"):list():append() end, desc = "harpoon file", },
-      { "<leader>f", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
-      { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1", },
-      { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2", },
-      { "<leader>3", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3", },
-      { "<leader>4", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4", },
-      { "<leader>P", function() require("harpoon"):list():prev() end, desc = "harpoon to prev file", },
-      { "<leader>N", function() require("harpoon"):list():next() end, desc = "harpoon to next file", },
+      {
+        "<leader>F",
+        function()
+          require("harpoon"):list():append()
+        end,
+        desc = "harpoon file",
+      },
+      {
+        "<leader>f",
+        function()
+          local harpoon = require "harpoon"
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "harpoon quick menu",
+      },
+      {
+        "<leader>1",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "harpoon to file 1",
+      },
+      {
+        "<leader>2",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "harpoon to file 2",
+      },
+      {
+        "<leader>3",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "harpoon to file 3",
+      },
+      {
+        "<leader>4",
+        function()
+          require("harpoon"):list():select(4)
+        end,
+        desc = "harpoon to file 4",
+      },
+      {
+        "<leader>P",
+        function()
+          require("harpoon"):list():prev()
+        end,
+        desc = "harpoon to prev file",
+      },
+      {
+        "<leader>N",
+        function()
+          require("harpoon"):list():next()
+        end,
+        desc = "harpoon to next file",
+      },
     },
   },
 
@@ -37,43 +98,53 @@ return {
     },
   },
 
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
 
-
-  -- These are some examples, uncomment them if you want to see them work!
-   {
-     "neovim/nvim-lspconfig",
-     config = function()
-       require("nvchad.configs.lspconfig").defaults()
-       require "configs.lspconfig"
-     end,
-   },
-
-   {
-   	"williamboman/mason.nvim",
-   	opts = {
-   		ensure_installed = {
-   			"lua-language-server", "stylua", "eslint-lsp",
-   			"html-lsp", "css-lsp" , "prettier",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "eslint-lsp",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
 
         -- c/cpp
-        "clangd", "clang-format",
+        "clangd",
+        "clang-format",
 
         -- shell
         "shfmt",
         "pyright",
-        "rust-analyzer"
+        "rust-analyzer",
+      },
+    },
+  },
 
-   		},
-   	},
-   },
-
-   {
-   	"nvim-treesitter/nvim-treesitter",
-   	opts = {
-   		ensure_installed = {
-   			"vim", "lua", "vimdoc", "typescript", "tsx", "c", "markdown", "markdown_inline",
-        "html", "css", "javascript",
-   		},
-   	},
-   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "typescript",
+        "tsx",
+        "c",
+        "markdown",
+        "markdown_inline",
+        "html",
+        "css",
+        "javascript",
+      },
+    },
+  },
 }
